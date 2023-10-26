c88c2a78dc20d9a7f1ad591c365a9bcc04b615ca
Vyacheslav Erokhin <er.vyacheslav2023@gmail.com>
2023-10-26 13:08:08 +0300

    added homework

---
+++ src/test/java/tests/RegistrationTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTest extends TestBase {
+
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").setValue("Vyacheslav");
+        $("#lastName").setValue("Erokhin");
+        $("#userEmail").setValue("er.vyacheslav2023@gmail.com");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("9508628655");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-dropdown-container").$(byText("March")).click();
+        $(".react-datepicker__year-select").selectOption("1992");
+        $(".react-datepicker__day--004").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("1.png");
+        $("#currentAddress").setValue("Currect Address");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Rajasthan")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Jaipur")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(
+                text("Vyacheslav Erokhin"),
+                text("er.vyacheslav2023@gmail.com"),
+                text("Male"),
+                text("9508628655"),
+                text("04 March,1992"),
+                text("Computer Science"),
+                text("Sports"),
+                text("1.png"),
+                text("Currect Address"),
+                text("Rajasthan Jaipur")
+        );
+    }
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,18 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+
+    }
+
+}
